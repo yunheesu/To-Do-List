29a5502eea2b80a5f0e7a28217d8560f4cf78e21
Heesu Yun <Yunheesu@icloud.com>
2020-02-03 00:51:10 -0500

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Heesu Yun on 2/3/20.
+//  Copyright © 2020 Heesu Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
